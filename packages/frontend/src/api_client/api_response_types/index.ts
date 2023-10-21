281af58b3d374c8687fb9e4998c30e170e3c327d
Jason Cheung <59743969+Jason0429@users.noreply.github.com>
2023-10-21 11:02:25 -0400

    Create api client (#8)

---
+++ packages/frontend/src/api_client/api_response_types/index.ts
@@ -0,0 +1,3 @@
+export interface SomeAuthenticatedRouteResponse {
+  message: string;
+}
---
+++ packages/frontend/src/api_client/index.ts
@@ -0,0 +1,47 @@
+import Axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { SomeAuthenticatedRouteResponse } from './api_response_types';
+
+export class APIClient {
+  private readonly baseURL: string = 'http://localhost:3001/api';
+
+  private static instance: APIClient;
+  private axios: AxiosInstance;
+
+  private constructor() {
+    this.axios = Axios.create({
+      baseURL: this.baseURL,
+      headers: { 'content-type': 'application/json' }
+    });
+  }
+
+  public static getInstance(): APIClient {
+    if (!APIClient.instance) {
+      APIClient.instance = new APIClient();
+    }
+
+    return APIClient.instance;
+  }
+
+  /**
+   * Authenticated routes
+   */
+  auth = {
+    someAuthenticatedRoute: (token: string) => {
+      return this.req<SomeAuthenticatedRouteResponse>({
+        method: 'POST',
+        url: '/user/someAuthenticatedRoute',
+        headers: { Authorization: `Bearer ${token}` }
+      });
+    }
+  };
+
+  private async req<T>(config: AxiosRequestConfig): Promise<T> {
+    const res = await this.axios.request(config);
+
+    if (res.status != 200) {
+      throw new Error(res.statusText);
+    }
+
+    return res.data as T;
+  }
+}
