70e84c72716ffce8fb14e6418b2a647557792d32
Jason0429 <jasoncheung0429@gmail.com>
2023-10-21 11:01:41 -0400

    removed unnecessary check

--- packages/backend/src/modules/user/user.controller.ts
+++ packages/backend/src/modules/user/user.controller.ts
@@ -2,16 +2,7 @@ import { Request, Reply } from '../../types/server';
 import verifyIdToken from '../../utils/verify_id_token';
 
 const someAuthenticatedHandler = async (request: Request, reply: Reply) => {
-  const token = request.headers.authorization?.split('Bearer ')[1];
-
-  if (!token) {
-    reply.code(401).send('Invalid token');
-    return;
-  }
-
-  const uid = await verifyIdToken(token);
-
-  // Do something with uid
+  return reply.send({ message: "You're authenticated!" });
 };
 
 export default someAuthenticatedHandler;
--- packages/backend/src/modules/user/user.service.ts
+++ packages/backend/src/modules/user/user.service.ts
@@ -1,7 +1,7 @@
 import prisma from '../../utils/prisma_client';
 import { CreateUserInput } from './user.schema';
 
-const createUser = async (input: CreateUserInput) => {
+export const createUser = async (input: CreateUserInput) => {
   const user = await prisma.user.create({
     data: input
   });
