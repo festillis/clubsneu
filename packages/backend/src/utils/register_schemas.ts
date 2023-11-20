d65c7832ba65a922cfc5c255fce5c0b4ba30e652
Jason0429 <jasoncheung0429@gmail.com>
2023-11-20 18:52:37 -0500

    monolithic

--- packages/backend/src/utils/register_schemas.ts
+++
@@ -1,9 +0,0 @@
-import { JsonSchema } from 'fastify-zod';
-import { Server } from '../types/server';
-
-// Register schemas
-export const registerSchemas = (server: Server, schemas: Array<JsonSchema>) => {
-  for (const schema of schemas) {
-    server.addSchema(schema);
-  }
-};
