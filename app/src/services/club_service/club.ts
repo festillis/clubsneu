3c3442c8811bd15742c0c7b0e0ae75416fcda1af
Jason0429 <jasoncheung0429@gmail.com>
2023-11-29 18:34:46 -0500

    format

--- app/src/services/club_service/club.ts
+++ app/src/services/club_service/club.ts
@@ -1,6 +1,7 @@
 import { Prisma } from '@prisma/client';
 import { prisma } from '~/prisma';
 
+// Must run server-side
 const getClubById = async (id: string) => {
   const club = await prisma.club.findUnique({
     where: { id }
@@ -8,23 +9,27 @@ const getClubById = async (id: string) => {
   return club;
 };
 
+// Must run server-side
 export const clubExists = async (id: string) => {
   const club = await getClubById(id);
   return !!club;
 };
 
+// Must run server-side
 export const createClub = async (input: Prisma.ClubCreateInput) => {
   return await prisma.club.create({
     data: input
   });
 };
 
+// Must run server-side
 export const deleteClub = async (id: string) => {
   return await prisma.club.delete({
     where: { id }
   });
 };
 
+// Must run server-side
 export const addOwnerToClub = async (clubId: string, userId: string) => {
   return await prisma.club.update({
     where: { id: clubId },
@@ -34,6 +39,19 @@ export const addOwnerToClub = async (clubId: string, userId: string) => {
           id: userId
         }
       }
+    }
+  });
+};
+
+// Must run server-side
+export const updateClubCalendarUrl = async (
+  clubId: string,
+  calendarUrl: string
+) => {
+  return await prisma.club.update({
+    where: { id: clubId },
+    data: {
+      calendarUrl
     }
   });
 };
