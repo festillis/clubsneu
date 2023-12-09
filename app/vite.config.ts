c466efb900e2fa5d6b98225a6ee0ba5e1124a2cb
Jason0429 <jasoncheung0429@gmail.com>
2023-12-08 19:18:37 -0500

    added vercel adapter

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import suidPlugin from '@suid/vite-plugin';
 import solidSvg from 'vite-plugin-solid-svg';
+import vercel from 'solid-start-vercel';
 
 export default defineConfig({
   plugins: [
     solid({
-      ssr: false // currently disabled for hydration mismatch error when using SVGs
+      ssr: false, // currently disabled for hydration mismatch error when using SVGs
+      adapter: vercel()
     }),
     suidPlugin(),
     solidPlugin(),
